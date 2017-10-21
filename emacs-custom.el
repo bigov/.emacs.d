9879e72b7112bb0dd10fc1d69cd3362bc457852e
bigov <balezin@yandex.ru>
2017-10-21 14:11:45 +0900

    some fix

--- emacs-custom.el
+++ emacs-custom.el
@@ -43,6 +43,10 @@
 
 ;;Автодополнение - company, company-go и добавляем в конфиг: 
 (require 'company)
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
 
 ;;Сниппеты
 (require 'yasnippet)
