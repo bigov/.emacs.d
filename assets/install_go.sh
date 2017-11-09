44c13c9bb3c4ce631d1b0f43026c5668b1e5af55
Igor Balezin <balezin@yandex.ru>
2017-11-09 16:55:47 +0900

    fix install go description

---
+++ assets/install_go.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/jstemmer/gotags
+go get -u github.com/kisielk/errcheck
+go get -u golang.org/x/tools/cmd/guru
+go get -u github.com/golang/lint/golint
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/godoc
+
